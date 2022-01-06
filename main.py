b4279ad94b665d4c55a0599883c574836bd28667
Vladislav Zaripov <vladislavzaripov.vz@gmail.com>
2022-01-06 11:33:21 +0500

    added rest methods

--- main.py
+++ main.py
@@ -16,3 +16,30 @@ app.add_middleware(
 @app.get('/')
 def read_root():
     return {'ping': 'pong'}
+
+
+@app.get('/api/todo')
+async def get_todo():
+    return 1
+
+
+@app.get('/api/todo{title}')
+async def get_todo_by_title(title):
+    return 1
+
+
+@app.post('/api/todo')
+async def post_todo(todo):
+    return 1
+
+
+@app.put('/api/todo{title}')
+async def put_todo(title, data):
+    return 1
+
+
+@app.delete('/api/todo{title}')
+async def delete_todo(todo):
+    return 1
+
+
